bf7071611a621e6c8b70f78f3a3159de93060acf
Ali Almohaya <alialmohaya@gmail.com>
2018-02-23 02:01:16 +0300

    extract the mapers to an external file

---
+++ src/app/thread-section/mapers.ts
@@ -0,0 +1,23 @@
+import { ApplicationState } from "../store/application-state";
+import { ThreadSummaryVM } from "./threadSummary.vm";
+import { Thread } from "../../../shared/model/thread";
+import { values } from "lodash";
+
+export const mapStateToUserName = (state: ApplicationState): string => {
+  const { userId: currentUserId } = state.uiState;
+  return state.dataState.participants[currentUserId].name;
+}
+
+export const mapStateToUnReadMessages = (state: ApplicationState): number => {
+  const { userId: currentUserId } = state.uiState;
+  return values<Thread>(state.dataState.threads)
+    .reduce((acc, thread: Thread) => acc + thread.participants[currentUserId], 0);
+}
+
+
+export const mapStateToThreadSummary = (): ThreadSummaryVM[] => {
+
+
+
+  return;
+}
--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import { values } from 'lodash';
 
 import { ThreadsService } from '../services/threads.service';
 
 import { ApplicationState } from '../store/application-state';
 import { LoadUserThreadAction } from '../store/actions';
 import { Observable } from 'rxjs/observable';
-import { Thread } from '../../../shared/model/thread';
+import { ThreadSummaryVM } from './threadSummary.vm';
+import { mapStateToUserName, mapStateToUnReadMessages, mapStateToThreadSummary } from './mapers';
 
 @Component({
   selector: 'thread-section',
   templateUrl: './thread-section.component.html',
-  styleUrls: ['./thread-section.component.css']
+  styleUrls: ['./thread-section.component.scss']
 })
 export class ThreadSectionComponent implements OnInit {
 
   userName$: Observable<string>;
   unreadMessages$: Observable<number>;
+  threadSummarys$: Observable<ThreadSummaryVM[]>;
 
   constructor(private threadService: ThreadsService,
     private store: Store<ApplicationState>) { }
@@ -32,25 +33,15 @@ export class ThreadSectionComponent implements OnInit {
 
     this.userName$ = this.store
       .skip(1) // to skip the first init emit
-      .map(this.mapStateToUserName);
+      .map(mapStateToUserName);
 
     this.unreadMessages$ = this.store
       .skip(1)
-      .map(this.mapStateToUnReadMessages);
+      .map(mapStateToUnReadMessages);
 
+    this.threadSummarys$ = this.store
+      .skip(1)
+      .map(mapStateToThreadSummary);
   }
-
-
-  private mapStateToUserName(state: ApplicationState): string {
-    const { userId: currentUserId } = state.uiState;
-    return state.dataState.participants[currentUserId].name;
-  }
-
-  private mapStateToUnReadMessages(state: ApplicationState): number {
-    const { userId: currentUserId } = state.uiState;
-    return values<Thread>(state.dataState.threads)
-      .reduce((acc, thread: Thread) => acc + thread.participants[currentUserId], 0);
-  }
-
 
 }
