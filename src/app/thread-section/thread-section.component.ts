ca5506f808184c0efbf241293cbef6e134492c08
Ali Almohaya <alialmohaya@gmail.com>
2018-02-23 01:39:01 +0300

    set the username and unread message with map state

--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
+import { values } from 'lodash';
+
 import { ThreadsService } from '../services/threads.service';
 
 import { ApplicationState } from '../store/application-state';
 import { LoadUserThreadAction } from '../store/actions';
+import { Observable } from 'rxjs/observable';
+import { Thread } from '../../../shared/model/thread';
 
 @Component({
   selector: 'thread-section',
@@ -13,7 +17,8 @@ import { LoadUserThreadAction } from '../store/actions';
 })
 export class ThreadSectionComponent implements OnInit {
 
-  userName: string;
+  userName$: Observable<string>;
+  unreadMessages$: Observable<number>;
 
   constructor(private threadService: ThreadsService,
     private store: Store<ApplicationState>) { }
@@ -25,10 +30,27 @@ export class ThreadSectionComponent implements OnInit {
         .dispatch(new LoadUserThreadAction(allUserData)));
 
 
-    this.store.subscribe((state: ApplicationState) => {
-      this.userName = state.dataState.participants[state.uiState.userId].name;
-    });
+    this.userName$ = this.store
+      .skip(1) // to skip the first init emit
+      .map(this.mapStateToUserName);
+
+    this.unreadMessages$ = this.store
+      .skip(1)
+      .map(this.mapStateToUnReadMessages);
 
   }
+
+
+  private mapStateToUserName(state: ApplicationState): string {
+    const { userId: currentUserId } = state.uiState;
+    return state.dataState.participants[currentUserId].name;
+  }
+
+  private mapStateToUnReadMessages(state: ApplicationState): number {
+    const { userId: currentUserId } = state.uiState;
+    return values<Thread>(state.dataState.threads)
+      .reduce((acc, thread: Thread) => acc + thread.participants[currentUserId], 0);
+  }
+
 
 }
