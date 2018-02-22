77f42ab05192238075a8e54422a2825f5101e83c
Ali Almohaya <alialmohaya@gmail.com>
2018-02-22 11:48:20 +0300

    add some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { MessageListComponent } from './message-list/message-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent,
+    ThreadListComponent,
+    MessageListComponent,
   ],
   imports: [
     BrowserModule
