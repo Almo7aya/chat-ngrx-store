a446d2380b509ce629b22f6888de70f8fd759915
Ali Almohaya <alialmohaya@gmail.com>
2018-03-02 19:16:17 +0300

    add router state model

--- src/app/store/application-state.ts
+++ src/app/store/application-state.ts
@@ -4,11 +4,12 @@ import { DataState, INITIAL_DATA_STATE } from './data-state';
 import { UiState, INITIAL_UI_STATE } from './ui-state';
 import { Route, Params, RouterStateSnapshot } from '@angular/router';
 import { dbMessagesQueuePerUser } from '../../server/model/data-db';
+import { RouterState } from './router-state';
 
 export interface ApplicationState {
   dataState: DataState;
   uiState: UiState;
-  routerState: RouterReducerState;
+  routerState: RouterReducerState<RouterState>;
 }
 
 export const INITIAL_APPLICATION_STATE: ApplicationState = {
