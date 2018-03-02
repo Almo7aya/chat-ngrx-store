474d6ea9eb3b56cc802f44ace8c2367003185743
Ali Almohaya <alialmohaya@gmail.com>
2018-03-02 13:04:51 +0300

    add error message

--- src/app/error-messages/error-messages.component.ts
+++ src/app/error-messages/error-messages.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ApplicationState } from '../store/application-state';
+import { Observable } from 'rxjs/observable';
 
 @Component({
-  selector: 'error-messages',
+  selector: 'app-error-messages',
   templateUrl: './error-messages.component.html',
   styleUrls: ['./error-messages.component.css']
 })
 export class ErrorMessagesComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('class.show') showMessage: boolean;
+
+  message$: Observable<String>;
+
+  constructor(private store: Store<ApplicationState>) { }
 
   ngOnInit() {
+    this.message$ = this.store.select(state => state.uiState.currentError)
+      .skip(1)
+      .do(() => {
+        this.showMessage = true;
+        setTimeout(() => this.showMessage = false, 2000);
+      });
+  }
+
+  close() {
+    this.showMessage = false;
   }
 
 }
