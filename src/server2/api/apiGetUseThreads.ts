61587282b339bfa988726c8eef4197d190e679f9
Ali Almohaya <alialmohaya@gmail.com>
2018-03-05 18:12:35 +0300

    add getThreadsByParticipantId function

--- src/server2/api/apiGetUseThreads.ts
+++ src/server2/api/apiGetUseThreads.ts
@@ -1,8 +1,16 @@
 import { Application, Response, Request } from 'express';
+import { getThreadsByParticipantId } from '../persistence/getThreadsByParticipantId';
 
 export const apiGetUserThreads = (app: Application) => {
 
   app.route('/apiv2/threads').get((req: Request, res: Response) => {
+
+    const participantId = 1; // just for now
+
+    const participantThreads = getThreadsByParticipantId(participantId);
+
+    res.writeHead(200, { 'Content-Type': 'text/plan' });
+    res.end('Its working');
 
   });
 
--- src/server2/server.ts
+++ src/server2/server.ts
@@ -1,12 +1,17 @@
 import * as express from 'express';
 import { Application } from 'express';
+import { apiGetUserThreads } from './api/apiGetUseThreads';
 
 const bodyParser = require('body-parser');
 
 
 const app: Application = express();
 
-app.use(bodyParser);
+app.use(bodyParser.json());
+
+
+apiGetUserThreads(app);
+
 
 app.listen(8090, () => {
   console.log('Server is working at Port 8090');
