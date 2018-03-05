4b2ac6cee084463dcab0e76758286747e8e59b14
Ali Almohaya <alialmohaya@gmail.com>
2018-03-05 19:06:42 +0300

    fully add the apiGetUserthreads route

--- src/server2/api/apiGetUseThreads.ts
+++ src/server2/api/apiGetUseThreads.ts
@@ -2,8 +2,9 @@ import { Application, Response, Request } from 'express';
 import { getDbThreadsByParticipantId } from '../persistence/getDbThreadsByParticipantId';
 import { Thread } from '../../../shared/model/thread';
 import { Message } from '../../../shared/model/message';
-import { filter, values, keys } from 'lodash';
-import { dbMessages } from '../../server/model/data-db';
+import { AllUserData } from '../../../shared/to/all-user-data';
+import { filter, values, keys, uniq } from 'lodash';
+import { dbMessages, dbParticipants } from '../../server/model/data-db';
 
 export const apiGetUserThreads = (app: Application) => {
 
@@ -13,17 +14,25 @@ export const apiGetUserThreads = (app: Application) => {
 
     const participantThreads: Thread[] = getDbThreadsByParticipantId(participantId);
 
-    const messages: Message[] = [];
-    const participantIds: string[] = [];
+    let messages: Message[] = [];
+    let participantIds: string[] = [];
 
     participantThreads.forEach((thread: Thread) => {
       const threadMessages: Message[] = filter(values(dbMessages), (message: Message) => message.threadId === thread.id);
       const threadParticipantIds: string[] = keys(thread.participants);
-      messages.concat(threadMessages);
-      participantIds.concat(threadParticipantIds);
+      messages = [...messages, ...threadMessages];
+      participantIds = [...participantIds, ...threadParticipantIds];
     });
 
-    res.status(200).json({ messages, participantIds });
+    const participants = uniq(participantIds.map(partId => dbParticipants[partId]));
+
+    const response: AllUserData = {
+      messages,
+      participants,
+      threads: participantThreads
+    };
+
+    res.status(200).json(response);
 
   });
 
